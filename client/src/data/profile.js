b4ab08bed5f8e81b03aa6f5e6ea5945055445761
Emiyelan <emakaringe65@gmail.com>
2026-02-03 01:08:55 +0200

    Refactor to Static Site (Zero-Backend)

---
+++ client/src/data/profile.js
@@ -0,0 +1,9 @@
+export const profile = {
+    name: 'Emmanuel Makaringe',
+    title: 'Software Engineer',
+    email: 'Emakaringe65@gmail.com',
+    phone: '067 693 7921',
+    github: 'https://github.com/Emiyelan',
+    linkedin: 'https://www.linkedin.com/in/emmanuel-makaringe-753033216',
+    bio: 'Passionate developer proficient in the MERN stack. I love building web applications that solve real-world problems.'
+};
